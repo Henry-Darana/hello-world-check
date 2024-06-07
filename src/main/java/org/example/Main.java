bb37e7b46efd9a1acd9294783832fad10aec0229
henry <henry.darana@tray.com>
2024-06-07 14:56:43 +0530

    Initial Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class Main {
+
+    @Test
+    public void checkHelloWorld() {
+        System.out.println("Hello world!");
+    }
+}
